9060fc80d57794c8fbbc88ed631ec5ac4a8a2da3
AlGaRitm2020 <albert.gareev.2018@yandex.ru>
2021-03-14 12:22:38 +0400

    login, register, show news(example flask + sqlite lesson)

--- data/users.py
+++ data/users.py
@@ -1,8 +1,10 @@
 import datetime
 import sqlalchemy
-from .db_session import SqlAlchemyBase
+# from .db_session import *
 from flask_login import UserMixin
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
 
 class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
@@ -16,4 +18,12 @@ class User(SqlAlchemyBase, UserMixin):
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
+
+    news = orm.relation("News", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
 
